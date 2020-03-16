ef4bfed3d829d2651fd4ef3a960d761b30d78581
Austin-s-h <44502805+Austin-s-h@users.noreply.github.com>
2020-03-16 15:31:51 -0400

    Adding silent miniconda install

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 cd ~
 #Install miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-sh Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+source ~/.bashrc
 #Add channels, order is important!
 conda config --add channels defaults
 conda config --add channels bioconda
