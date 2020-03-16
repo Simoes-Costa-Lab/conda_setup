637dc8c305866812e56ab87d5cdbd857d90ad0b1
Austin-s-h <44502805+Austin-s-h@users.noreply.github.com>
2020-03-16 16:29:35 -0400

    Fixing conda init and default py37

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd ~
 #Install miniconda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
-eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+$HOME/miniconda3/bin/conda init
 source ~/.bashrc
 #Add channels, order is important!
 conda config --add channels defaults
