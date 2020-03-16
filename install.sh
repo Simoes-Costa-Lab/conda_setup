c30b55968bdf502e2607a5dfd50cb3a657e699c1
Austin-s-h <44502805+Austin-s-h@users.noreply.github.com>
2020-03-16 15:00:57 -0400

    Initial configuration for conda environment

---
+++ install.sh
@@ -0,0 +1,14 @@
+#Default location is home directory install
+cd ~
+#Install miniconda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+sh Miniconda3-latest-Linux-x86_64.sh
+#Add channels, order is important!
+conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+#Create new environments from the yml files
+conda env create -f py27environment.yml
+#py37 is created last to become the default
+conda env create -f py37environment.yml
+conda activate py37
